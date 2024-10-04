4963aa49d16327718d23dcac235e10dc55dd19e7
Choi1999 <dprtldk157@gmail.com>
2024-10-04 13:44:28 +0900

    5번

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -3,24 +3,29 @@ package org.example.expert.config;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 class PasswordEncoderTest {
 
-    @InjectMocks
+
+    @InjectMocks //=  Mockito 프레임워크에서 사용되는 어노테이션으로
+    //테스트 클래스에서 의존성 주입을 자동으로 처리해주는 역할을 한다.
     private PasswordEncoder passwordEncoder;
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
+
         // given
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        boolean matches = passwordEncoder.matches( rawPassword,encodedPassword);//matches() 메서드는 첫 번째 인수로 평문 비밀번호를, 두 번째 인수로 암호화된 비밀번호를 받아서 비교
+
 
         // then
         assertTrue(matches);
